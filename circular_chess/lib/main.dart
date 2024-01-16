6518b53af3360a0726b781888abcf715546aceb6
PrettyBoyFlacko <123202427+PrettyBoyFlacko@users.noreply.github.com>
2024-01-16 15:03:36 -0700

    Update main.dart
    Added the chess board

--- circular_chess/lib/main.dart
+++ circular_chess/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:circular_chess/screens/wrapper.dart';
+import 'package:circular_chess/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:circular_chess/models/user.dart';
 
-void main() async{
+Future <void> main()async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+//Test email: test@gmail.com
+//Test password: test12345678
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,8 +20,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<myUser?>.value(
+      initialData: null,
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
